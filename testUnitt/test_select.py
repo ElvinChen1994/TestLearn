ca88edc20463d56b176ac189476500ef5ed9f6f3
macintoshhd <18310066396@163.com>
2020-03-26 00:44:33 +0800

    测试查询

---
+++ testUnitt/test_select.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+# @Time: 2020/3/13 9:10 下午
+# @Author: Elvin
+import pytest
+
+
+import pytest
+
+
+@pytest.fixture
+def error_fixture():
+    assert 0
+
+
+def test_ok():
+    print("ok")
+
+
+def test_fail():
+    assert 0
+
+
+def test_error(error_fixture):
+    pass
+
+
+def test_skip():
+    pytest.skip("skipping this test")
+
+
+def test_xfail():
+    pytest.xfail("xfailing this test")
+
+
+@pytest.mark.xfail(reason="always xfail")
+def test_xpass():
+    pass
