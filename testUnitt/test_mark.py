db170a94535142fdbb3b6664ec0576d668288666
macintoshhd <18310066396@163.com>
2020-03-31 10:29:44 +0800

    测试request

--- testUnitt/test_mark.py
+++ testUnitt/test_mark.py
@@ -72,3 +72,25 @@ $ pytest -k “not send_http” -v
 
 
 
+import pytest
+
+@pytest.mark.webtest
+def test_send_http():
+    print("mark web test")
+
+def test_something_quick():
+    pass
+
+def test_another():
+    pass
+
+@pytest.mark.hello
+class TestClass:
+    def test_01(self):
+        print("hello :")
+
+    def test_02(self):
+        print("hello world!")
+
+if __name__ == "__main__":
+    pytest.main(["-v", "test_mark.py", "-m=hello"])
